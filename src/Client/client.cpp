c91729f14d2a1b01cc250ab8f588ffdba4ddf590
Damien <damien.demontis@epitech.eu>
2022-09-19 18:22:40 +0200

    [ADD] Cmake installing sfml (NO COMPLETE DEPENDENCIES) and compiling a working binary that opens a sfml window

---
+++ src/Client/client.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2022
+** B-CPP-500-NCY-5-1-rtype-clovis.schneider
+** File description:
+** main of Client
+*/
+
+#include "client.hpp"
+
+int main(int argc, char **argv)
+{
+    sf::Window window(sf::VideoMode(800, 600), "Rutabaga-Type");
+
+    return(0);
+}
---
+++ src/Client/client.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2022
+** indie
+** File description:
+** IEngine
+*/
+
+#ifndef CLIENT_HPP_
+#define CLIENT_HPP_
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <list>
+
+#include <SFML/Window.hpp>
+
+#endif /* !CLIENT_HPP_ */
