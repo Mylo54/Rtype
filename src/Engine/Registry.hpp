0c6395b00f4b2df5bdf355fcf0a26f0ecb3f2078
schneider <Yobobis@gmail.com>
2022-09-25 09:30:23 +0200

    [ADD] Registry start

--- src/Engine/Registry.hpp
+++ src/Engine/Registry.hpp
@@ -9,20 +9,32 @@
 #define REGISTRY_HPP_
 
 #include "Entity.hpp"
-
+#include "SparseArray.hpp"
+#include <unordered_map>
+#include <typeindex>
+#include <any>
 
 namespace eng
 {
     class Registry {
         public:
-            Registry();
-            ~Registry();
+            Registry() {}
+            ~Registry() {}
 
-            Entity spawnEntity();
-            Entity entityFromIndex(size_t index);
-            void killEntity(Entity const &e);
+            // Adds a new component array to the associatives container
+            template <typename Component>
+            void registerComponents(SparseArray<Component> &a) {
+                _containers[typeid(Component)] = a;
+            }
+
+            // Retrieve the array storing a Component 
+            template <typename Component>
+            SparseArray<Component> &getComponents(std::type_index t) {
+                return _containers[t];
+            }
         protected:
         private:
+            std::unordered_map<std::type_index, std::any> _containers;
     };
 } // namespace eng
 
