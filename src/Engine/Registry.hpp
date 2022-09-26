b82e23ffca6bb13e48832ca6a289654676502e72
schneider <Yobobis@gmail.com>
2022-09-26 19:10:28 +0200

    [ADD] completed registerComponents() and completed instructions for getComponents()

--- src/Engine/Registry.hpp
+++ src/Engine/Registry.hpp
@@ -24,22 +24,32 @@ namespace eng
             // Adds a new component array to the associatives container
             template <typename Component>
             void registerComponents(SparseArray<Component> &a) {
-                // Cast the SparseArray<Component> into SparseArray<std::any>
-                // using std::any_cast
-                _containers[typeid(Component)] = a;
+                _containers[typeid(SparseArray<Component>)] = getAnyArray<Component>(a);
             }
 
-            // Retrieve the array storing a Component
+            // Retrieve a reference to the array storing Components
             template <typename Component>
             SparseArray<Component> &getComponents(std::type_index t) {
                 // Cast the SparseArray<std::any> into SparseArray<Component>
                 // using std::any_cast
-                SparseArray<Component> res = std::any_cast(_containers[t].)
-                // Return the casted array
-                return res;
+                // return a **REFERENCE** OF THE ARRAY IN THE MAP casted in Component type
             }
+
         protected:
         private:
+            // Returns a copy of the sparse array with the std::any type
+            template <typename Component>
+            eng::SparseArray<std::any> getAnyArray(eng::SparseArray<Component> &a)
+            {
+                eng::SparseArray<std::any> res;
+                int i = 0;
+
+                for (auto it = a.begin(); it != a.end(); i++, it++) {
+                    res.insertAt(i, it->value());
+                }
+                return (res);
+            }
+
             std::unordered_map<std::type_index, SparseArray<std::any>> _containers;
     };
 } // namespace eng
