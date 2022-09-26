488703d9c63cabe07dbe58a71a4ed8207b8cd2ba
schneider <Yobobis@gmail.com>
2022-09-26 18:33:04 +0200

    [ADD] added comments with instructions for registry class

--- src/Engine/Registry.hpp
+++ src/Engine/Registry.hpp
@@ -24,17 +24,23 @@ namespace eng
             // Adds a new component array to the associatives container
             template <typename Component>
             void registerComponents(SparseArray<Component> &a) {
+                // Cast the SparseArray<Component> into SparseArray<std::any>
+                // using std::any_cast
                 _containers[typeid(Component)] = a;
             }
 
             // Retrieve the array storing a Component
             template <typename Component>
             SparseArray<Component> &getComponents(std::type_index t) {
-                return _containers[t];
+                // Cast the SparseArray<std::any> into SparseArray<Component>
+                // using std::any_cast
+                SparseArray<Component> res = std::any_cast(_containers[t].)
+                // Return the casted array
+                return res;
             }
         protected:
         private:
-            std::unordered_map<std::type_index, std::any> _containers;
+            std::unordered_map<std::type_index, SparseArray<std::any>> _containers;
     };
 } // namespace eng
 
