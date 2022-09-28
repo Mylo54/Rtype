9a567f753c65ea1c4a371917f1127751272838f1
Vivant Garrigues <vgarrigues@localhost.localdomain>
2022-09-28 18:20:37 +0200

    [ADD] loadPrefs and loadPref

--- src/Engine/PlayerPrefs.cpp
+++ src/Engine/PlayerPrefs.cpp
@@ -7,6 +7,7 @@
 
 #include "PlayerPrefs.hpp"
 #include <fstream>
+#include <regex>
 
 eng::PlayerPrefs::PlayerPrefs()
 {
@@ -16,9 +17,33 @@ eng::PlayerPrefs::~PlayerPrefs()
 {
 }
 
+void eng::PlayerPrefs::loadPref(const std::string str)
+{
+    std::regex keyRgx("(\\w+) .*");
+    std::regex valueRgx(".*\\[(\\w+)\\].*");
+    std::smatch match;
+    std::string key = "";
+    std::string value = "";
+
+    if (std::regex_search(str.begin(), str.end(), match, keyRgx))
+        key = match[1];
+    if (std::regex_search(str.begin(), str.end(), match, valueRgx))
+        value = match[1];
+
+    _strmap.insert(std::pair<std::string, std::string>(key, value));
+}
+
 void eng::PlayerPrefs::loadPrefs()
 {
-    
+    std::ifstream file;
+    std::string str;
+
+    file.open(_path);
+    while (std::getline(file, str)) {
+        // std::cout << str << std::endl;
+        loadPref(str);
+    }
+    file.close();
 }
 
 void eng::PlayerPrefs::setPref(std::string key, std::string value)
@@ -38,9 +63,18 @@ void eng::PlayerPrefs::setPref(std::string key, float value)
 
 std::string eng::PlayerPrefs::getPref(std::string key)
 {
-    std::ifstream saveFile;
+    if (_strmap.count(key) > 0)
+        return _strmap[key];
+        // return "azs";
+    return "Key not found";
+}
 
-    saveFile.open("PlayerPreferences.txt");
-    // get the string with the key associated
-    return "";
+void eng::PlayerPrefs::setPath(std::string path)
+{
+    _path = path;
+}
+
+std::string eng::PlayerPrefs::getPath()
+{
+    return _path;
 }
