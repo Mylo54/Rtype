a5a9897cd56ae2ea5ebae1437fc9eac8035bc941
Vivant Garrigues <vgarrigues@localhost.localdomain>
2022-09-28 16:54:25 +0200

    [UP] PlayerPrefs new methods

--- src/Engine/PlayerPrefs.cpp
+++ src/Engine/PlayerPrefs.cpp
@@ -16,48 +16,31 @@ eng::PlayerPrefs::~PlayerPrefs()
 {
 }
 
-void eng::PlayerPrefs::setString(std::string key, std::string value)
+void eng::PlayerPrefs::loadPrefs()
+{
+    
+}
+
+void eng::PlayerPrefs::setPref(std::string key, std::string value)
 {
     this->_strmap[key] = value;
 }
 
-void eng::PlayerPrefs::setInt(std::string key, int value)
+void eng::PlayerPrefs::setPref(std::string key, int value)
 {
-    this->_intmap[key] = value;
+    // this->_intmap[key] = value;
 }
 
-void eng::PlayerPrefs::setFloat(std::string key, float value)
+void eng::PlayerPrefs::setPref(std::string key, float value)
 {
-    this->_floatmap[key] = value;
+    // this->_floatmap[key] = value;
 }
 
-std::string eng::PlayerPrefs::getString(std::string key)
+std::string eng::PlayerPrefs::getPref(std::string key)
 {
     std::ifstream saveFile;
 
     saveFile.open("PlayerPreferences.txt");
     // get the string with the key associated
     return "";
-}
-
-float eng::PlayerPrefs::getFloat(std::string key)
-{
-    // Same as getString
-}
-
-int eng::PlayerPrefs::getInt(std::string key)
-{
-    // Same as getString
-}
-
-bool eng::PlayerPrefs::save()
-{
-    std::ofstream saveFile;
-    std::map<std::string, int>::iterator intIt;
-
-    saveFile.open("PlayerPreferences.txt");
-    saveFile << "[IntMap]" << std::endl;
-    for (intIt = this->_intmap.begin(); intIt != this->_intmap.end(); intIt++) {
-        saveFile << intIt->first << " = " << intIt->second << std::endl;
-    }
 }
