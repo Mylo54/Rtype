10376e3a0f033c66342e86dd7e4de9660016fad2
ChloeLere <chloe.lere@epitech.eu>
2022-10-05 13:21:37 +0200

    [EDIT] lobbyManager -> add basic fonction to getnbr add and rm lobby in a list

--- src/Game/Server/LobbyManager.cpp
+++ src/Game/Server/LobbyManager.cpp
@@ -15,3 +15,24 @@ rt::LobbyManager::LobbyManager()
 rt::LobbyManager::~LobbyManager()
 {
 }
+
+int rt::LobbyManager::getNumberLobby()
+{
+    return (this->_listLobby.size());
+}
+
+void rt::LobbyManager::addLobby(rt::Lobby lobby)
+{
+    this->_listLobby.push_back(lobby);
+}
+
+void rt::LobbyManager::removeLobby(int position)
+{
+    std::list<rt::Lobby>::iterator it = this->_listLobby.begin();
+
+    for (int i = 0; i <= position; i++, it++) {
+        if (it != this->_listLobby.end())
+            return;
+    }
+    this->_listLobby.erase(it);
+}
--- src/Game/Server/LobbyManager.hpp
+++ src/Game/Server/LobbyManager.hpp
@@ -7,6 +7,10 @@
 
 #ifndef LOBBY_MANAGER_SERVER_HPP_
 #define LOBBY_MANAGER_SERVER_HPP_
+#include "Lobby.hpp"
+#include <list>
+#include <iterator>
+#include <iostream>
 
 namespace rt {
     class LobbyManager {
@@ -14,10 +18,21 @@ namespace rt {
             LobbyManager();
             ~LobbyManager();
 
-            
+            /// @brief get the number of lobby in the list
+            /// @return int
+            int getNumberLobby();
+
+            /// @brief add lobby at the end of the list
+            /// @return void
+            void addLobby(rt::Lobby lobby);
+
+            /// @brief remove lobby in the list depending on his position
+            /// @return void
+            void removeLobby(int position);
 
         protected:
         private:
+            std::list<rt::Lobby> _listLobby;
             
     };
 };
