a54107c15c16817bc2af301ea0f728634a8a9234
Vivant Garrigues <vgarrigues@localhost.localdomain>
2022-10-05 11:35:15 +0200

    [ADD] log class + RegistryManager

--- src/Client/main.cpp
+++ src/Client/main.cpp
@@ -9,11 +9,9 @@
 #include "../Components/Components.hpp"
 #include "../Systems/Systems.hpp"
 #include "../Engine/Registry.hpp"
-#include "../Engine/Engine.hpp"
+#include "../Engine/RegistryManager.hpp"
+#include "../Engine/Log.hpp"
 #include <string.h>
-
-std::string logPath = "";
-extern std::string logPath;
 
 eng::Registry createRegistry(bool isDebugMode, std::string name)
 {
@@ -32,41 +30,29 @@ eng::Registry createRegistry(bool isDebugMode, std::string name)
     return (res);
 }
 
-void newLogPath(void)
-{
-    time_t now = time(0);
-    tm* ltm = localtime(&now);
-    logPath = std::to_string(ltm->tm_mday) + "-" +
-    std::to_string(1 + ltm->tm_mon) + "-" + std::to_string(1900 + ltm->tm_year)
-    + "_" + std::to_string(5 + ltm->tm_hour) + "h"
-    + std::to_string(ltm->tm_min) + "m" + std::to_string(ltm->tm_sec)
-    + "s.log";
-    std::ofstream out(logPath.c_str());
-    out << "Log from this run:\n";
-    out.close();
-}
-
 int main(int argc, char **argv)
 {
-    eng::Registry reg;
     sf::Clock c;
     rtp::Systems systems;
+    eng::RegistryManager engine;
+    eng::Registry reg = createRegistry(true, "Registry7");
+    eng::Log log;
 
-    reg.setName("Registry1");
+    engine.setDebugMode(false);
     for (int i = 1; i < argc; i++)
         if (strcmp(argv[i], "-debug") == 0) {
-            reg.setDebugMode(true);
-            newLogPath();
+            engine.setDebugMode(true);
+            engine.setLogPath(log.createPath());
         }
-    reg = createRegistry(true, reg.getName());
-    eng::Entity baba = reg.spawnEntity();
+    engine.addRegistry(reg);
+    eng::Entity baba = engine.getRegistries().top().spawnEntity();
+
     sf::RenderWindow w(sf::VideoMode(1920, 1080, 32), "Rutabaga");
     w.setFramerateLimit(60);
-
-    reg.addComponent<rtp::Position>(baba, rtp::Position(0, 0, 0));
-    reg.addComponent<rtp::Velocity>(baba, rtp::Velocity(0, 0));
-    reg.addComponent<rtp::Drawable>(baba, rtp::Drawable("../assets/platypus_spaceship.png", w, c, 1, sf::IntRect(0, 0, 65, 49), 0.005));
-    reg.addComponent<rtp::Controllable>(baba, rtp::Controllable(w));
+    engine.getRegistries().top().addComponent<rtp::Position>(baba, rtp::Position(0, 0, 0));
+    engine.getRegistries().top().addComponent<rtp::Velocity>(baba, rtp::Velocity(0, 0));
+    engine.getRegistries().top().addComponent<rtp::Drawable>(baba, rtp::Drawable("../assets/platypus_spaceship.png", w, c, 1, sf::IntRect(0, 0, 65, 49), 0.005));
+    engine.getRegistries().top().addComponent<rtp::Controllable>(baba, rtp::Controllable(w));
 
     while (w.isOpen())
     {
@@ -76,9 +62,9 @@ int main(int argc, char **argv)
             if (event.type == sf::Event::Closed)
                 w.close();
         }
-        systems.controlSystem(reg);
-        systems.positionSystem(reg);
-        systems.drawSystem(reg);
+        systems.controlSystem(engine.getRegistries().top());
+        systems.positionSystem(engine.getRegistries().top());
+        systems.drawSystem(engine.getRegistries().top());
     }
     return 0;
 }
