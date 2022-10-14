a480987bcc807f2ea13ab5b951fc012b9e13f298
Damien <damien.demontis@epitech.eu>
2022-10-14 15:18:47 +0200

    [ADD] added --config RELEASE to discord info message, github action, and compilation scripts

--- comp.sh
+++ comp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd build && conan install .. --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True && cmake .. -G "Unix Makefiles"  && cmake --build .
+mkdir -p build && cd build && conan install .. --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True && cmake .. -G "Unix Makefiles"  && cmake --build . --config RELEASE 
--- comp_windows.sh
+++ comp_windows.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 cd build && conan install .. --build=missing  && cmake .. -G "Visual Studio 17"  -A x64 && cmake --build . --config RELEASE
+
+
+
