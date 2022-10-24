3d8a67fe6f2d0cb9585eba706a93923c4fed8c8c
schneider <Yobobis@gmail.com>
2022-10-24 20:13:05 +0200

    [ADD] submodule pull on window compilation script

--- comp_windows.sh
+++ comp_windows.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+git submodule update --init --recursive
+mkdir -p build
 cd build && conan install .. --build=missing  && cmake .. -G "Visual Studio 17"  -A x64 && cmake --build . --config RELEASE
