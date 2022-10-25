8debe6ea6a92b41033ff91913a36f58d0024c8f5
dam <damien.demontis@epitech.eu>
2022-10-25 13:55:30 +0200

    [ADD] comp_windows now pulls submodules

--- comp_windows.sh
+++ comp_windows.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-conan profile update settings.compiler.libcxx=libstdc++11 default
 git submodule update --init --recursive
+git submodule update --recursive --remote
 mkdir -p build
 cd build && conan install .. --build=missing  && cmake .. -G "Visual Studio 17"  -A x64 && cmake --build . --config RELEASE
