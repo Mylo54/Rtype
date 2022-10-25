36080dba8cbcd487236934d17ed95e92b2de3367
dam <damien.demontis@epitech.eu>
2022-10-25 13:56:12 +0200

    [ADD] comp_linux with submodules

--- comp_linux.sh
+++ comp_linux.sh
@@ -2,6 +2,7 @@
 
 conan profile update settings.compiler.libcxx=libstdc++11 default
 git submodule update --init --recursive
+git submodule update --recursive --remote
 mkdir -p build
 cd build
 conan install .. --build=missing -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
