0e9287e62f38a0202ddc91c8b4d65f7a81b5195c
schneider <Yobobis@gmail.com>
2022-10-25 13:57:05 +0200

    [UP] endlines

--- comp_windows.sh
+++ comp_windows.sh
@@ -3,4 +3,7 @@
 git submodule update --init --recursive
 git submodule update --recursive --remote
 mkdir -p build
-cd build && conan install .. --build=missing  && cmake .. -G "Visual Studio 17"  -A x64 && cmake --build . --config RELEASE
+cd build
+conan install .. --build=missing
+cmake .. -G "Visual Studio 17"  -A x64
+cmake --build . --config RELEASE
