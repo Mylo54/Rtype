c8f0ece0add2d0d62985db8ef992054a2360b623
schneider <Yobobis@gmail.com>
2022-11-08 14:07:33 +0100

    [ADD] init client

--- src/Client/Client.cpp
+++ src/Client/Client.cpp
@@ -7,10 +7,36 @@
 
 #include "Client.hpp"
 
-rtp::Client::Client()
+rtp::Client::Client(): _graphics(1920, 1080, "Super R-Type"),
+_inputs(_graphics.getRenderWindow()), _physics(_graphics.getDeltaSeconds())
 {
+    std::cout << "Client has been created" << std::endl;
 }
 
 rtp::Client::~Client()
 {
+}
+
+int rtp::Client::run()
+{
+    _sceneEvent = sceneEvent::none;
+    _sceneNumber = sceneNumber::menu;
+    _registries.addRegistry("start");
+
+    std::cout << "Client is up!" << std::endl;
+    while (_graphics.isWindowOpen()) {
+        // this will be replaced by scenes.top().run;
+        {
+            _graphics.eventCatchWindow();
+            _graphics.clear();
+            _graphics.display();
+        }
+
+        // change scene & registry when event;
+        if (_sceneEvent != rtp::sceneEvent::none) {
+            _registries.addRegistry("new");
+            //scenes.push(askedscene);
+        }
+    }
+    return (0);
 }
--- src/Client/Client.hpp
+++ src/Client/Client.hpp
@@ -8,15 +8,45 @@
 #ifndef CLIENT_HPP_
 #define CLIENT_HPP_
 
+#include <stack>
+#include <EngineCoreSuper/EngineCoreSuper.hpp>
+#include "../Network/Network.hpp"
+#include "../Scene/IScene.hpp"
+
 namespace rtp
 {
+    enum sceneEvent{
+        none,
+        popScene,
+        pushScene,
+    };
+
+    enum sceneNumber{
+        game,
+        menu,
+        pause
+        // ...others
+    };
+
     class Client {
         public:
             Client();
             ~Client();
 
+            /// @brief run the Client
+            /// @return 0 on success, 84 on program failure
+            int run();
         protected:
         private:
+            int _sceneEvent;
+            int _sceneNumber;
+            eng::RegistryManager _registries;
+            eng::SuperInput _inputs;
+            eng::PhysicSystems _physics;
+            eng::GraphicSystems _graphics;
+            eng::AudioSystems _audio;
+            //rtp::Network _net;
+            std::stack<rtp::IScene> _scenes;
     };
 } // namespace rtp
 
